7acd29e2e04be9be0a0fe39d181f1efdfb0ae311
Dana-G <giordano.dana@gmail.com>
2014-10-15 18:32:54 -0400

    added data to popups, commented out bootstrap headers, updated css

--- app/assets/javascripts/parking_spots.js
+++ app/assets/javascripts/parking_spots.js
@@ -1,9 +1,13 @@
 
 $(document).ready( function(){
 
+var map;
+
 var initmap = function(){
 
-    var map = L.map('map').setView([39.9522, -75.1639], 16);
+    map =  L.map('map').setView([39.9522, -75.1639], 16);
+
+
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -13,12 +17,34 @@ var initmap = function(){
     // L.marker([39.9522, -75.1639]).addTo(map)
     //     .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
     //     .openPopup();
-    // }
+
+    };
 
     initmap();
-    $(".marker_link").click(function(){
-        var lat = $(this).parent().parent().find(".lat").text();
-        var lon = $(this).parent().parent().find(".lon").text();
+
+    $("tr").each(function(){
+        var lat = $(this).find(".lat").text();
+        var lon = $(this).find(".lon").text();
+        var street = $(this).find(".street").text();
+        var side = $(this).find(".side").text();
+        var spots = $(this).find(".spots").text();
+
+
+        if( lat == "" || lon == "" ){
+
+        }
+        else{
+           L.marker([lat, lon]).addTo(map)
+           .bindPopup((street + ", ") + side + " side" + ", " + spots + " spots.")
+           .openPopup();
+        }
 
     });
-} );
+
+
+    // $(".marker_link").click(function(){
+    //     var lat = $(this).parent().parent().find(".lat").text();
+    //     var lon = $(this).parent().parent().find(".lon").text();
+
+    // });
+});
